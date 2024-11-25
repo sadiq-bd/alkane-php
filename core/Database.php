f7e130ccf5d09f643efa9db67f2e90ca2d1c4a5a
Sadiq Ahmed <sadiq0698@gmail.com>
2024-11-26 01:56:18 +0600

    Update

--- core/Database.php
+++ core/Database.php
@@ -2,25 +2,21 @@
 
 namespace Core;
 
-use \PDO;
-use \PDOException;
+use PDO;
+use PDOException;
 
 /**
  * Database Class
  *
  * @category  Database Access
  * @package   Database
- * @author    Sadiq <sadiq.developer.bd@gmail.com>
- * @copyright Copyright (c) 2022-23
- * @version   2.0
- * @package   Alkane\Database
+ * @author    Sadiq <sadiq.dev.bd@gmail.com>
+ * @version   2.2
  */
-
 class Database {
 
     /**
-     * @var $config
-     * Default configurations are stored in an associative array
+     * Default configurations.
      */
     private static $config = [
         'type' => 'mysql',
@@ -36,40 +32,36 @@ class Database {
     ];
 
     /**
-     * custom configurations
-     * @var array
+     * Custom configurations.
      */
     private static $custom_configs = [];
 
     /**
-     * @var $pdo
-     * connection storing property
+     * Stores PDO connections.
      */
-    private static $pdo = array();
+    private static $pdo = [];
 
     /**
-     * @var $current_connection
-     * current connection name
+     * Current connection name.
      */
     private $current_connection = 0;
 
     /**
-     * @var on_error_conn
-     * callback function
+     * Error callback.
      */
     private static $on_error_conn = null;
 
     /**
-     * Error Modes in an associative array
+     * Error modes map.
      */
     protected static $errMode = [
         'silent' => PDO::ERRMODE_SILENT,
         'warning' => PDO::ERRMODE_WARNING,
         'exception' => PDO::ERRMODE_EXCEPTION
     ];
-  
+
     /**
-     * Fetch Modes in an associative array
+     * Fetch modes map.
      */
     protected static $fetchMode = [
         'assoc' => PDO::FETCH_ASSOC,
@@ -78,299 +70,158 @@ class Database {
     ];
 
     /**
-     * @var errorInfo
-     * Stores error informations
+     * Stores error information.
      */
     private $errorInfo = '';
 
     /**
-     * Constructor of the class
-     * @param info
-     * Addition configurations can be pass through it
+     * Constructor to initialize the connection.
      */
-    public function __construct(string $coustom_config_name = '') {
-        // set current connection name
-        $this->current_connection = $coustom_config_name !== '' ? $coustom_config_name : 0;
-
-        // Connect to the database
-        $this->connect($coustom_config_name);
+    public function __construct(string $custom_config_name = '') {
+        $this->current_connection = $custom_config_name ?: 0;
+        $this->connect($custom_config_name);
     }
 
     /**
-     * set host
+     * Set configuration dynamically.
      */
-    public static function setHost($host) {
-        self::$config['host'] = $host;
-    }
-
-    /**
-     * set user
-     */
-    public static function setUser($user) {
-        self::$config['user'] = $user;
-    }
-
-    /**
-     * set password
-     */
-
-    public static function setPassword($password) {
-        self::$config['password'] = $password;
-
-    }
-
-    /**
-     * set dbname
-     */
-
-    public static function setDbname($dbname) {
-        self::$config['dbname'] = $dbname;
-
-    }
-
-    /**
-     * set port
-     */
-
-    public static function setPort($port) {
-        self::$config['port'] = $port;
-    }
-
-    /**
-     * set charset
-     */
-
-    public static function setCharset($charset) {
-        self::$config['charset'] = $charset;
-
-    }
-
-    /**
-     * set errmode
-     */
-
-    public static function setErrmode($errmode) {
-        self::$config['errmode'] = $errmode;
-    }
-
-    /**
-     * set fetch_mode
-     */
-    
-    public static function setFetchMode($fetchMode) {
-        self::$config['fetch_mode'] = $fetchMode;
-    }
-    
-    /**
-     * set emulate_prepares
-     */
-
-    public static function setEmulatePrepares($emulate_prepares) {
-        self::$config['emulate_prepares'] = $emulate_prepares;
-    }
-
-    /**
-     * @param name
-     * Name (key) of the config
-     * @param value
-     * Value (value) of the config
-     */
-    public static function setConfig(string $name, $value) {
-        if (isset(self::$config[$name])) {
+    public static function setConfig(string $name, $value): void {
+        if (array_key_exists($name, self::$config)) {
             self::$config[$name] = $value;
         }
     }
 
     /**
-     * Add custom configurations
+     * Add custom configuration.
      */
-    public static function addCustomConfig(string $name, array $config) {
+    public static function addCustomConfig(string $name, array $config): void {
         self::$custom_configs[$name] = $config;
     }
 
     /**
-     * @param name
-     * Name (key) of the config
-     * @return value
-     * Returns Config Value
+     * Retrieve a configuration value.
      */
     public static function getConfig(string $name) {
-        if (isset(self::$config[$name])) {
-            return self::$config[$name];
-        } else {
-            return false;
-        }
+        return self::$config[$name] ?? false;
     }
 
     /**
-     * @param $info
-     * create config array
+     * Validate a configuration array.
      */
-    public function createConfig($info) {
-        $config = [];
-        foreach (self::$config as $key => $val) {
-            if (isset($info[$key])) {
-                $config[$key] = $info[$key];
-            } else {
-                $config[$key] = $val;
-            }
-        }
-        return $config;
+    private static function validateConfig(array $config): bool {
+        return isset($config['type'], $config['host'], $config['user'], $config['password']);
     }
 
     /**
-     * checks config
-     * @param $config
+     * Create a merged configuration array.
      */
-    public static function checkConfig($config) {
-        if (
-            isset($config['type']) && 
-            isset($config['host']) && 
-            isset($config['user']) && 
-            isset($config['password'])
-            ) {
-            return true;
-        } else {
-            return false;
-        }
+    private function createConfig(array $custom_config): array {
+        return array_merge(self::$config, $custom_config);
     }
 
-
     /**
-     * Creates a database connection with PDO
-     * @param $coustom_config_name
-     * Custom configurations can be pass through it
+     * Establish a PDO connection.
      */
-    public function connect(string $coustom_config_name = '') {
-        // Setup the config
-        if (!empty($coustom_config_name)) {
-            if (!isset(self::$custom_configs[$coustom_config_name])) {
-                trigger_error('Database custom configuration \'' . $coustom_config_name . '\' not found', E_USER_ERROR);
-            }
-            $config = $this->createConfig(self::$custom_configs[$coustom_config_name]);
-            $i = $coustom_config_name;
-        } else {
-            $config = $this->createConfig(self::$config);
-            $i = 0;
+    private function connect(string $custom_config_name = ''): void {
+        $config = $custom_config_name 
+            ? ($this->createConfig(self::$custom_configs[$custom_config_name] ?? []))
+            : $this->createConfig(self::$config);
+
+        if (!self::validateConfig($config)) {
+            throw new PDOException('Incomplete Database Configuration.');
         }
 
-        // Check if config is valid
-        if (!self::checkConfig($config)) {
-            trigger_error('Incomplete Database Configuration', E_USER_ERROR);
-        }
-
-        // if everything is ok then ready to connect
         try {
-
-            $dsn = $config['type'];
-            $dsn .= ':host=' . $config['host']; 
-            $dsn .= ';port=' . $config['port']; 
-            if (!empty($config['dbname']) && $config['dbname'] != '*') {
-                $dsn .= ';dbname=' . $config['dbname'];
+            $dsn = "{$config['type']}:host={$config['host']};port={$config['port']};charset={$config['charset']}";
+            if (!empty($config['dbname']) && $config['dbname'] !== '*') {
+                $dsn .= ";dbname={$config['dbname']}";
             }
-            $dsn .= ';charset=' . $config['charset'];
-            
-            // Create a new PDO instance
-            self::$pdo[$i] = new PDO($dsn, $config['user'], $config['password']);
-            
-            self::$pdo[$i]->setAttribute(
-                PDO::ATTR_DEFAULT_FETCH_MODE , 
-                self::$fetchMode[
-                    $config['fetch_mode']
-                ]);
-            self::$pdo[$i]->setAttribute(
-                PDO::ATTR_ERRMODE , 
-                self::$errMode[
-                    $config['errmode']
-                ]);
-            self::$pdo[$i]->setAttribute(
-                PDO::ATTR_EMULATE_PREPARES , 
-                $config[
-                    'emulate_prepares'
-                ]);
 
+            self::$pdo[$this->current_connection] = new PDO($dsn, $config['user'], $config['password']);
+            $pdo = self::$pdo[$this->current_connection];
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, self::$fetchMode[$config['fetch_mode']]);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, self::$errMode[$config['errmode']]);
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, $config['emulate_prepares']);
         } catch (PDOException $e) {
-            if (self::$on_error_conn != null) {
-                if (is_callable(self::$on_error_conn)) {
-                    call_user_func_array(self::$on_error_conn, [
-                        $e->getMessage()
-                    ]);  
-                }
-            } 
+            if (self::$on_error_conn && is_callable(self::$on_error_conn)) {
+                call_user_func(self::$on_error_conn, $e->getMessage());
+            } else {
+                throw $e;
+            }
         }
     }
 
     /**
-     * @return object $pdo
+     * Retrieve the active PDO connection.
      */
-    public function getConnection(string $name = '') {
-        if (!empty($name)) {
-            return self::$pdo[$name];
-        } else {
-            return self::$pdo[$this->current_connection];
-        }
+    public function getConnection(string $name = ''): ?PDO {
+        return self::$pdo[$name ?: $this->current_connection] ?? null;
     }
 
     /**
-     * get Database class instance
-     * @param $coustom_config_name
-     * @return object
+     * Static method to retrieve an instance.
      */
-    public static function getInstance($coustom_config_name = '') {
-        return new self($coustom_config_name);
+    public static function getInstance(string $custom_config_name = ''): self {
+        return new self($custom_config_name);
     }
 
     /**
-     * Executes Custom Query
-     * @param string $sql
-     * Sql Query
-     * @param array $data
-     * additional Param data
+     * Execute a SQL query with parameters.
      */
-    public function query_exec(string $sql, array $data = []) {
+    public function executeQuery(string $sql, array $params = []): ?PDOStatement {
         try {
             $conn = $this->getConnection();
-            $conn->beginTransaction();
+            if (!$conn) {
+                throw new PDOException('Database connection not established.');
+            }
+
             $stmt = $conn->prepare($sql);
-            $exec = $stmt->execute($data);
-            $conn->commit();
+            $stmt->execute($params);
             return $stmt;
         } catch (PDOException $e) {
-            $conn->rollBack();
+            $this->errorInfo .= $e->getMessage();
+            return null;
+        }
+    }
+
+    /**
+     * Set a custom error callback.
+     */
+    public static function onErrorConnection(callable $func): void {
+        self::$on_error_conn = $func;
+    }
+
+    /**
+     * Retrieve the error information.
+     */
+    public function getErrorInfo(): string {
+        return $this->errorInfo;
+    }
+
+    /**
+     * Test if the connection is active.
+     */
+    public function testConnection(): bool {
+        try {
+            $conn = $this->getConnection();
+            return $conn && $conn->query('SELECT 1');
+        } catch (PDOException $e) {
             $this->errorInfo .= $e->getMessage();
             return false;
         }
     }
-    
+
     /**
-     * @param $func
-     * sets event on error connection
+     * Close a specific connection.
      */
-    public static function onErrorConnection($func) {
-        if (is_callable($func)) {
-            self::$on_error_conn = $func;
-        }
-    }
-           
-    /**
-     * @return string $errorInfo
-     */
-    public function getErrorInfo() {
-        return $this->errorInfo;
-    }
-         
-    /**
-     * Closes PDO connection
-     */
-    public function closeConnection() {
-        self::$pdo[$this->current_connection] = null;
+    public function closeConnection(string $name = ''): void {
+        self::$pdo[$name ?: $this->current_connection] = null;
     }
 
     /**
-     * Destructor of the class
+     * Destructor to clean up.
      */
     public function __destruct() {
         $this->closeConnection();
     }
-
 }
